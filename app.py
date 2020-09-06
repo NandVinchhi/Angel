b58b4cb6ee343ad84778979bcdc53383f89b083d
NandVinchhi <nand.vinchhi@gmail.com>
2020-09-07 01:14:35 +0530

    init

---
+++ app.py
@@ -0,0 +1,97 @@
+from pymongo import MongoClient
+from pymongo.collection import ObjectId
+from random import randint
+from datetime import datetime
+import json
+
+client = MongoClient("mongodb+srv://user:pwd@cluster0.pqawv.mongodb.net/<dbname>?retryWrites=true&w=majority")
+
+db = client.get_database("data")
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+def generate_id():
+    final = ""
+    c = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for i in range(0, 6):
+        k = randint(0, 35)
+        final += c[k]
+
+    return final
+
+def register_patient(name, strictness, fbid):
+    userid = generate_id()
+    k = {"name":name, "strictness":strictness, "fbid":fbid, "userid":userid}
+    try:
+        db.users.insert_one(k)
+        return {"status":"success", "userid":userid}
+    except:
+        return {"status":"failed"}
+
+def get_patient_data(userid):
+    try:
+        return JSONEncoder().encode(db.users.find_one({"userid":userid}))
+    except :
+        return {"status":"failed"}
+#print(register_patient("Nand", 0.5, "213908"))
+
+#print(get_patient_data("ng8ax9"))
+
+def regenerate_id(userid):
+    try:
+        new_id = generate_id()
+        db.users.update_one({"userid":userid}, {"$set":{"userid":new_id}})
+        return {"status":"success", "new_id":new_id}
+    except:
+        return {"status":"failed"}
+
+def add_prescription(userid, name, daily_count, num_days, times):
+    d = datetime.now().date()
+    date = d.day
+    month = d.month
+    year = d.year
+    pres_id = generate_id()
+    k = {"userid":userid, "pres_id":pres_id, "name":name, "daily_count":daily_count, "num_days":num_days, "times":times, "remaining":daily_count, "day":date, "month":month, "year":year}
+    try:
+        db.pres.insert_one(k)
+        return {"status":"success", "pres_id":pres_id}
+    except:
+        return {"status":"failed"}
+
+def get_pres(name):
+    try:
+        return JSONEncoder().encode(db.pres.find_one({"name":name}))
+    except :
+        return {"status":"failed"}
+
+def get_all_pres(userid):
+    try:
+        final = []
+        for i in db.pres.find({"userid":userid}):
+            final.append(i)
+        return JSONEncoder().encode({"status":"success", "result":final})
+    except :
+        return {"status":"failed"}
+#print(add_prescription("ng8ax9", "crocin", 3, 14, [1200, 1300, 1400]))
+#print(get_pres("2nozps"))
+
+def update_pres(name, inc):
+    try:
+        db.pres.update_one({"name":name}, {"$inc":{"remaining":inc}})
+        return {"status":"success"}
+    except:
+        return {"status":"failed"}
+
+def reset_pres(name):
+    try:
+        db.pres.update_one({"name":name}, {"$set":{"remaining":db.pres.find_one({"name":name})["daily_count"]}})
+        return {"status":"success"}
+    except:
+        return {"status":"failed"}
+
+#print(reset_pres("crocin"))
