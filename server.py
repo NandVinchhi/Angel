c046465a43eb605035a631ef48d16bfa39305652
Veer Gadodia <veergadodia24@gmail.com>
2020-09-07 12:50:47 -0400

    main backend server for app

---
+++ server.py
@@ -0,0 +1,82 @@
+# INSTRUCTIONS
+# ngrok http 5000
+# Put https://51b6b3fa.ngrok.io/webhook url in the facebook app callback request along with verify token
+# FLASK_APP=server.py flask run
+# ANY QUESTIONS SEE: https://www.datacamp.com/community/tutorials/facebook-chatbot-python-deploy
+
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
+VERIFY_TOKEN = 'tnwYWccSeZUDoNBGik7anJfKUNvanQkj4LSmRoYfj0A='
+PAGE_ACCESS_TOKEN = 'EAAFAGMARZB58BAHP3B3aDuxj297IArfr1ZAmxY1TOGd1ZAuRa3oZBPnDX0F4HWaPE4cLlwEhCz8Elzra2NmSXNfgOx4BNGZBPXmxz7PiG3NEhB1nb5gIM75SmZA6PxIoeeZCF8ZBNAzZAULEi8WWRZAhOzgLcEPC6evZA6ZA1NZA4ZCuFv5r4MLzZCANX8ZADpacZC8O0qlMZD'
+
+
+def get_bot_response(message):
+    """This is just a dummy function, returning a variation of what
+    the user said. Replace this function with one connected to chatbot."""
+    return "This is a dummy response to '{}'".format(message)
+
+
+def verify_webhook(req):
+    if req.args.get("hub.verify_token") == VERIFY_TOKEN:
+        return req.args.get("hub.challenge")
+    else:
+        return "incorrect"
+
+def respond(sender, message):
+    """Formulate a response to the user and
+    pass it on to a function that sends it."""
+    response = get_bot_response(message)
+    send_message(sender, response)
+
+
+def is_user_message(message):
+    """Check if the message is a message from the user"""
+    return (message.get('message') and
+            message['message'].get('text') and
+            not message['message'].get("is_echo"))
+
+def send_message(recipient_id, text):
+    """Send a response to Facebook"""
+    payload = {
+        'message': {
+            'text': text
+        },
+        'recipient': {
+            'id': recipient_id
+        },
+        'notification_type': 'regular'
+    }
+
+    auth = {
+        'access_token': PAGE_ACCESS_TOKEN
+    }
+
+    response = requests.post(
+        FB_API_URL,
+        params=auth,
+        json=payload
+    )
+
+    return response.json()
+
+@app.route("/webhook", methods=["GET", "POST"])
+def listen():
+    """This is the main function flask uses to 
+    listen at the `/webhook` endpoint"""
+    if request.method == 'GET':
+        return verify_webhook(request)
+
+    if request.method == 'POST':
+        payload = request.json
+        event = payload['entry'][0]['messaging']
+        for x in event:
+            if is_user_message(x):
+                text = x['message']['text']
+                sender_id = x['sender']['id']
+                respond(sender_id, text)
+
+        return "ok"
